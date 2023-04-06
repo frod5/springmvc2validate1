abc8012d17038f7bb928566fa2ad5f7bb97306b5
sunwooKim <frod5@kakao.com>
2023-04-06 21:21:13 +0900

    스프링 mvc 2편 - 검증2 V3 bean Validation 오브젝트 오류 실습

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -2,12 +2,14 @@ package hello.itemservice.domain.item;
 
 import lombok.Data;
 import org.hibernate.validator.constraints.Range;
+import org.hibernate.validator.constraints.ScriptAssert;
 
 import javax.validation.constraints.Max;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 @Data
+//@ScriptAssert(lang="javascript",script = "_this.price * _this.quantity >= 10000")  // 오브젝트 오류 확인. 되도록이면 이런 오브젝트 오류는 직접 자바코드로 하는것이 낫다.
 public class Item {
 
     private Long id;
